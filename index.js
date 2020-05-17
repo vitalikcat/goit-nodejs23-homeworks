03c4ff646e1ab8650249b0047575dc5a125a35bb
Vitalik <vitalik791@gmail.com>
2020-05-17 20:26:04 +0300

    added 02-express

--- index.js
+++ index.js
@@ -1,36 +1,105 @@
-const yargs = require("yargs");
-const data = require("./contacts");
+const service = require("./services/services");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const Joi = require("joi");
 
-const argv = yargs
-  .number("id")
-  .string("name")
-  .string("email")
-  .string("phone")
-  .alias("name", "n")
-  .alias("email", "e")
-  .alias("phone", "p").argv;
+const app = express();
+const PORT = 3001;
 
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      data.listContacts();
-      break;
+app.use(express.json());
+app.use(cors());
+app.use(morgan("combined"));
 
-    case "get":
-      data.getContactById(id);
-      break;
+app.get("/api/contacts", (req, res) => {
+  return res.status(200).send(service.listContacts());
+});
 
-    case "add":
-      data.addContact(name, email, phone);
-      break;
+app.get("/api/contacts/:contactId", (req, res) => {
+  const contactId = Number(req.params.contactId);
+  let foundContact = service.getById(contactId);
 
-    case "remove":
-      data.removeContact(id);
-      break;
-
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+  if (foundContact === undefined) {
+    res.status(404).json({ message: "Not found" });
+  } else {
+    res.status(200).send(foundContact);
   }
-}
+});
 
-invokeAction(argv);
+app.post(
+  "/api/contacts",
+  (req, res, next) => {
+    const validationRules = Joi.object({
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      phone: Joi.string().required(),
+    });
+
+    const validationResult = Joi.validate(req.body, validationRules);
+
+    if (validationResult.error) {
+      const requiredField = validationResult.error.details[0].path[0];
+
+      return res.status(400).json({
+        message: `missing required ${requiredField} field`,
+      });
+    } else {
+      next();
+    }
+  },
+  (req, res) => {
+    const contact = req.body;
+    let addedContact = service.addContact(contact);
+
+    res.status(201).send(addedContact);
+  }
+);
+
+app.delete("/api/contacts/:contactId", (req, res) => {
+  const contactId = Number(req.params.contactId);
+  let removedContact = service.removeContact(contactId);
+
+  if (removedContact === undefined) {
+    return res.status(404).json({ message: "Not found" });
+  } else {
+    return res.status(200).json({ message: "contact deleted" });
+  }
+});
+
+app.patch(
+  "/api/contacts/:contactId",
+  (req, res, next) => {
+    const validationRules = Joi.object({
+      name: Joi.string(),
+      phone: Joi.string(),
+      number: Joi.string(),
+    });
+    const result = Joi.validate(req.body, validationRules);
+
+    if (result.error || Object.keys(req.body).length === 0) {
+      return res.status(400).json({
+        message: "missing  fields",
+      });
+    }
+    next();
+  },
+  (req, res) => {
+    const contactId = Number(req.params.contactId);
+    const fieldsToUpdate = req.body;
+
+    let updatedContact = service.updateContact(contactId, fieldsToUpdate);
+
+    if (updatedContact === undefined) {
+      return res.status(404).json({ message: "Not found" });
+    } else {
+      return res.status(200).send(updatedContact);
+    }
+  }
+);
+
+app.listen(PORT, (error) => {
+  if (error) {
+    return console.log("something bad happened", error);
+  }
+  console.log("Server is runnig on port: ", PORT);
+});
